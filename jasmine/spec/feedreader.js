87db16c264f42c7a1f1e67fb708b60d5ef6f1864
Kevin Yang <ykwwind@gmail.com>
2016-03-25 00:21:51 -0700

    comments removed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,11 +26,6 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
          describe('Valid RSS Feed URL', function() {
             it('URL defined', function() {
                 allFeeds.forEach(function(feed){
@@ -45,10 +40,6 @@ $(function() {
             });
          });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
          describe('Valid RSS Feed Name', function() {
             it('Name defined', function() {
                 allFeeds.forEach(function(feed) {
@@ -63,47 +54,25 @@ $(function() {
          });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
-
     describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
         it('hidden by default', function() {
-            expect($('body').hasClass('menu-hidden')).toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();
         });
-        /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
         describe('visibility change', function() {
             beforeEach(function() {
                 $('.menu-icon-link').click();
             });
             it('Menu display', function() {
-                expect($('body').hasClass('menu-hidden')).not.toBe(true);
+                expect($('body').hasClass('menu-hidden')).toBeFalsy();
             });
             it('Menu hidden', function() {
-                expect($('body').hasClass('menu-hidden')).toBe(true);
+                expect($('body').hasClass('menu-hidden')).toBeTruthy();
             });
         });
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
         beforeEach(function(done) {
-            loadFeed(0, function() {
-                done();
-            });
+            loadFeed(0, done);
         });
         it('at least one entry after asynchronous call', function(done) {
             expect($('.entry-link').length).toBeGreaterThan(0);
@@ -112,21 +81,15 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
         var originHTMLContent;
         beforeEach(function(done) {
             loadFeed(0, function() {
                 originHTMLContent = $('.feed').html();
-                loadFeed(1, function() {
-                    done();
-                });
+                loadFeed(1, done);
             });
         });
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+
         it('New Feed added and content changes', function(done) {
             expect($('.feed').html()).not.toBe(originHTMLContent);
             done();
